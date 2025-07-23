fa25e2491d6b3bfc6b6fc05d2b4e38a14a46ff29
jiaofi <166228543+zhutishou@users.noreply.github.com>
2025-07-23 16:44:20 +0800

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <assert>
 
 namespace cx
 {
@@ -19,16 +19,75 @@ namespace cx
         T _val;//当前节点的值
     };
 
+    //定义迭代器部分：
+    template<class T,class Ref,class Ptr>
+    struct _list_iterator
+    {
+        typedef ListNode<T> Node;
+		typedef _list_iterator<T,Ref, Ptr> self;
+        //构造函数：
+        _list_iterator(Node* x)
+			:_node(x)
+		{}
+        //注意：无析构函数
+        //常见使用函数：
+        self& operator++()//前置++
+		{
+			_node = _node->_next;
+			return (*this);
+		}
+		self& operator++(int)//后置++
+		{
+			self tmp(*this);
+			_node = _node->_next;
+			return tmp;
+		}
+		self& operator--()//前置--
+		{
+			_node = _node->_prev;
+			return *this;
+		}
+		self& operator--(int)//后置--
+		{
+			self tmp(*this);
+			_node = _node->_prev;
+			return tmp;
+		}
+		Ref operator*()//Ref---是指解引用指向的内容
+		{
+			return _node->_val;
+		}
+		bool operator!=(const self& s)
+		{
+			return !(_node == s._node);
+		}
+		bool operator==(const self& s)
+		{
+			return !(*this != s);
+		}
+		Ptr operator->()//Ptr---是指地址
+		{
+			return &_node->_val;
+		}
+        //成员对象：
+        Node* _node;
+    };
+
     template<class T>
     class List 
     {
     public:
         typedef ListNode<T> Node;
+
+		typedef _list_iterator<T, T&, T*> iterator;
+		typedef _list_iterator<T,const T&,const T*> const_iterator;
+
         //构造函数：
         List()
         {
             empty_list();
         }
+
         //拷贝构造：
         List(const List<T>& list)
         {
@@ -38,6 +97,7 @@ namespace cx
                 push_back(e);//val             
             }
         }
+
         //运算符重载;
         List<T> operator=(const List<T>& list)
         {
@@ -54,18 +114,72 @@ namespace cx
             }
             return *this;
         }
+
         //析构函数：
         ~List()
         {
-            
+            clear();//清除有效数据部分
+            delete _head;
+            _head = nullptr;
         }
 
-
+        //迭代器部分；
+        iterator begin()//有效第一个节点
+		{
+            //可以检查下：
+            if(_head->_next)
+			    return _head->_next;
+		}
+		const_iterator begin() const
+		{
+            if(_head->_next)
+			    return _head->_next;
+		}
+		iterator end()
+		{
+			return _head;
+		}
+		const_iterator end() const
+		{
+			return _head;
+		}
 
         //modfifers:
+        iterator erase(iterator pos)
+		{
+			//检查pos位置非_head位置
+			if(pos ==end())
+                std::cerr<<"该位置不存在"<<std::endl;
+			//删除的是pos位置
+			Node* current = pos._node;
+			Node* prev = current->_prev;
+			Node* next = current->_next;
+			//删除操作
+			prev->_next = next;
+			next->_prev = prev;
+			delete current;
+			return next;//返回的是pos下一个位置
+		}
+		iterator erase(iterator first, iterator last)//区间左闭右开
+		{
+			assert(first != end());
+			assert(last != end());
+			while (first != last)
+			{
+				if (first == end())
+					break;
+				first = erase(first);
+			}
+			return first;
+		}
         void clear()
         {
-
+            //注意：clear只删除有效数据部分，不删除_head
+            iterator it = begin();
+            while(it != end())
+            {
+                it = erase(it);
+            }
         }
         void push_back(const T& val)
         {
@@ -98,6 +212,58 @@ namespace cx
             tmp->_prev = _head;
             _head->_next = tmp;
         }
+        bool empty() const
+		{
+			return _head->_next == _head;
+		}
+		size_t size() const
+		{
+			size_t n = 0;
+			Node* tmp = _head->_next;
+			while (tmp!=_head)
+			{
+				tmp = tmp->_next;
+				n++;
+			}
+			return n;
+		}
+        void swap(list<T>& tmp)
+		{
+			std::swap(_head, tmp._head);
+		}
+		iterator insert(iterator pos, const T& x)
+		{
+			//insert是指在pos位置前插入
+			Node* current = pos._node;
+			Node* prev = current->_prev;
+			//开空间
+			Node* tmp = new Node(x);
+			tmp->_prev = prev;
+			prev->_next = tmp;
+			tmp->_next = current;
+			current->_prev = tmp;
+			//return iterator(tmp);
+			return tmp;//注意点：单参数的类可以隐式类型转换
+		}
+
+        //Element access:
+		T& front()
+		{
+			return _head->_next->_val;
+		}
+		const T& front() const
+		{
+			return _head->_next->_val;
+		}
+		T& back()
+		{
+			return _head->_prev->_val;
+		}
+		const T& back() const
+		{
+			return _head->_prev->_val;
+		}
+
     private:
         void empty_list()
         {
